62451d9fff4436cdfdc464e4d03bec68fb84cacd
H3143 <1013230638@qq.com>
2025-04-05 10:21:00 +0800

    KKMW-2 feat:初步上传用户名片页面，需后续完善

---
+++ src/components/PlayerStatus/UserNameplates.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+  
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted,ref } from 'vue';
+  const userInfoBasic = ref({
+    Clan:'....',
+    NickName:'....',
+    Server:'asia',
+    UID:'....',
+    
+  })
+  
+  
+  
+  onMounted(() => {
+    
+  })
+  </script>
